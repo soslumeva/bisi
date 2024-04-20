dccfb9923ef6da0f9cf26fc7e3892a2b5eb693ba
soslumeva <165496530+soslumeva@users.noreply.github.com>
2024-04-20 19:06:26 +0300

    Create botbot.py

---
+++ botbot.py
@@ -0,0 +1,27 @@
+import discord
+
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$merhaba'):
+        await message.channel.send("Selam!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('$nasılsın'):
+        await message.channel.send("İyiyim")
+    else:
+        await message.channel.send(message.content)
+
+client.run("BURAYA GİR TOKENİ")
